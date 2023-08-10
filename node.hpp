c0b74caada4b45b4b6180df31a9cab10ee4a680e
RozAle <110930300+RuzannaAleksanyan@users.noreply.github.com>
2023-08-10 23:39:32 +0400

    Update node.hpp

--- node.hpp
+++ node.hpp
@@ -1,6 +1,5 @@
 #ifndef NODE
 #define NODE
-
 #include <string>
 #include <time.h>
 
@@ -8,18 +7,18 @@ class Node {
 private:
     std::string m_description;
     std::string m_status; 
-    time_t m_dedline;
+    time_t m_deadline;
 
 public:
     Node(const std::string& description, const std::string& status, time_t time);
 
     std::string get_description() const;
     std::string get_status() const;
-    time_t get_dedline() const;
+    time_t get_deadline() const;
 
     void set_description(const std::string& description);
     void set_status(const std::string& status);
-    void set_dedline(time_t time);
+    void set_deadline(time_t time);
 };
 
 #endif
